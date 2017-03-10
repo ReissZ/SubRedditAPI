a226d6f3c075aaee19009c0f89ea5290cdd5da92
Reiss Zurbyk <reiss.zurbyk@mobilelive.ca>
2017-03-10 13:33:37 -0500

    Initial Commit

---
+++ RedditAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditAPI
+//
+//  Created by Reiss Zurbyk on 2017-03-10.
+//  Copyright © 2017 Reiss Zurbyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
